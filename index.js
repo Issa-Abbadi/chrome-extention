9766afe47e351963c0b51cd3484c96368c518761
Issa Abbadi <eisaabbadi1@gmail.com>
2023-07-20 07:39:39 +0300

    keep the data stored

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
-localStorage.clear();
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
